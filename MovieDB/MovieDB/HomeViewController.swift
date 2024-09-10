360c7ae0a323a0549e29aad8b26a76e889492cbc
rfarbod <rfarbod@icloud.com>
2024-09-10 09:59:33 -0400

    Created the xcode project + set the default view on initial launch to home view controller

---
+++ MovieDB/MovieDB/HomeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  MovieDB
+//
+//  Created by Farbod Rahiminik on 9/10/24.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .orange
+    }
+}
+
